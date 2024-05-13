099ab438599b8f018e003b0054eb9887dfcf9546
Bernabe Suarez <bernatattoo@gmail.com>
2024-05-13 10:30:49 -0300

    js cards

---
+++ js/products.js
@@ -0,0 +1,202 @@
+const products = [
+    {
+        nombre: "Cannoli",
+        descripcion: `Experimente el icónico pastel italiano, el cannoli, una verdadera
+            delicia con su corteza crujiente y su delicioso relleno cremoso.
+            Originario de Sicilia, este querido postre ha deleitado el paladar
+            durante generaciones y sigue siendo un símbolo de la excelencia
+            culinaria italiana. < br /> La clave para un cannoli perfecto reside
+            en su delicada cáscara, un tubo de masa crujiente y frito que
+            encierra un relleno rico y cremoso. < br /> Tradicionalmente, el
+            relleno incluye queso ricotta, endulzado con azúcar y, en
+            ocasiones, adornado con chispas de chocolate o fruta confitada.
+            < br /> Con sus sabores y texturas irresistibles, los cannoli han
+            ganado popularidad en Italia y entre los entusiastas de los
+            postres en todo el mundo. < br /> Abrace el espíritu italiano y
+            saboree los cannoli rellenos de crema, un dulce atemporal y
+            querido que continúa alegrando los corazones de los amantes de los
+            postres de todo el mundo y sigue siendo un pastel popular.`,
+        img: "./img/cannoli.jpg"
+    },
+    {
+        nombre: "Baklava",
+        descripcion: `Deléitese con las ricas capas de nueces del Baklava, un postre
+            querido del Medio Oriente que ha resistido la prueba del tiempo
+            entre los pasteles más populares del mundo. < br /> Originario de
+            las cocinas reales del Imperio Otomano, este exquisito manjar está
+            elaborado con precisión utilizando delicada masa filo, que
+            envuelve un generoso relleno de nueces finamente picadas. < br /> Lo
+            que distingue al Baklava es el arte de remojar en jarabe de miel,
+        infundiendo a cada capa una deliciosa dulzura que equilibra
+            perfectamente la riqueza de las nueces.En todo Medio Oriente,
+        diferentes regiones le dan su toque único al Baklava, mostrando
+            una deliciosa variedad de sabores y texturas. < br /> El baklava es
+            un manjar preciado que continúa fascinando a los entusiastas de
+            los postres en todo el mundo con su deliciosa combinación de
+            hojaldre y el rico sabor de las nueces en cada bocado.`,
+        img: "./img/baklava.jpg"
+    },
+    {
+        nombre: "Croissant",
+        descripcion: ` El croissant, un renombrado pastel francés, tiene una rica
+            historia que se remonta a un período de influencia vienesa en
+            Francia y es uno de los pasteles más populares. <br />El origen
+            del croissant se remonta al siglo XVII, durante las guerras
+            otomano-Habsburgo, cuando los panaderos vieneses, famosos por sus
+            deliciosos y hojaldrados pasteles, utilizaron su experiencia
+            culinaria para crear lo que hoy conocemos como croissant. <br />A
+            principios del siglo XIX, el croissant llegó a Francia cuando
+            María Antonieta, miembro de la familia real austriaca, se casó con
+            Luis XVI de Francia. El Kipferl (nombre original) fue adaptado y
+            transformado en el ahora famoso croissant francés, ganando
+            popularidad en las panaderías parisinas. <br />La textura escamosa
+            y mantecosa del croissant es hoy una marca registrada de sus
+            ascendencia vienesa, y su enorme atractivo lo ha convertido en un
+            ícono de la habilidad pastelera francesa.`,
+        img: "./img/Croissants01.jpg"
+    },
+    {
+        nombre: "Danesas",
+        descripcion: `Deléitese con la delicia mantecosa de la pastelería danesa, un
+            delicioso desayuno favorito que ha capturado los corazones de los
+            entusiastas de la pastelería en todo el mundo. Originarias de
+            Dinamarca, estas delicias tienen su origen en el pan de Viena,
+            traído a Dinamarca por panaderos austriacos en el siglo XIX.
+            <br />El atractivo de los pasteles daneses radica en sus capas
+            ligeras y hojaldradas, perfectas para contener una variedad de
+            deliciosos rellenos de frutas. Desde frambuesa hasta albaricoque y
+            todo lo demás, la variedad de sabores es verdaderamente un placer
+            para el desayuno. <br />El queso crema danés es otra variedad muy
+            querida, que rezuma una delicia cremosa. A medida que los
+            panaderos continúan innovando, surgen nuevas variedades de
+            pastelería danesa, cada una de las cuales tienta el paladar a su
+            manera. <br />Despierte con el encantador aroma y sabor de las
+            delicias danesas rellenas de frutas, una delicia atemporal para el
+            desayuno entre los pasteles más populares del mundo.`,
+        img: "./img/danish.jpg"
+    },
+    {
+        nombre: "Eclairs",
+        descripcion: `Deléitese con la elegancia de los éclairs, un querido postre
+            francés conocido por su forma alargada y su deliciosa combinación
+            de masa choux, relleno cremoso y delicioso glaseado de chocolate.
+            Creado a partir de pâte à choux, una masa ligera y aireada, el
+            éclair cuenta con una capa exterior crujiente que encierra un
+            lujoso relleno de crema. <br />Estos deliciosos pasteles están
+            disponibles en una variedad de sabores, desde vainilla y chocolate
+            hasta selecciones más experimentales como diferentes tipos de café
+            y pistacho. <br />Cada éclair, cubierto con un brillante glaseado
+            de chocolate, es una obra de arte que tienta a los conocedores de
+            postres con su magnífico sabor y presentación. <br />Un símbolo
+            del arte pastelero francés, los éclairs continúan cautivando las
+            papilas gustativas y brindando alegría al dulce capricho. Saborea
+            los deliciosos éclairs rellenos de crema y deja que su encanto
+            francés te transporte a un mundo de perfección pastelera.`,
+        img: "./img/eclair.jpg"
+    },
+    {
+        nombre: "Macarons",
+        descripcion: `Ingrese al encantador mundo de los macarons, un dulce francés
+            delicado y colorido que ha cautivado a los entusiastas de los
+            postres en todo el mundo. <br />Los orígenes de este famoso pastel
+            y delicias se remontan al siglo VIII y se cree que fueron
+            introducidos por chefs italianos y luego refinados por los
+            talentosos pasteleros franceses. <br />El merengue a base de
+            almendras en el centro del macarrón es un componente crucial
+            responsable de su textura aireada característica. Hacer macarons
+            exige precisión y habilidad, convirtiendo cada uno en una obra de
+            belleza. <br />Los macarons no sólo son un festín visual con sus
+            colores vivos, sino que también tienen un perfecto equilibrio de
+            gustos. Un macarrón es adecuado para cualquier paladar, desde los
+            favoritos básicos como vainilla y chocolate hasta combinaciones
+            más experimentales como lavanda y caramelo salado. <br />Los
+            panaderos están experimentando con rellenos y presentaciones
+            estéticas novedosos, y las posibilidades creativas parecen
+            ilimitadas. Estas pequeñas obras de arte se han convertido en un
+            pilar de las pastelerías de todo el mundo, atrayendo a los amantes
+            de los postres con su atractivo irresistible. <br />Disfrutar de
+            un macarrón es como saborear un momento de puro deleite: un
+            equilibrio perfecto entre textura, sabor y belleza.`,
+        img: "./img/macaron.jpg"
+    },
+    {
+        nombre: "Mille feuille (Milhojas)",
+        descripcion: `El milhojas es un famoso postre francés famoso por sus delicadas
+            capas de hojaldre y su rica crema pastelera.Este plato, a menudo
+            conocido como pastelería de Napoleón, ejemplifica las habilidades
+            culinarias francesas. < br /> Desde el crujiente del hojaldre hasta
+            la sedosa suavidad de la crema pastelera, cada bocado de milhojas
+            revela una sinfonía de texturas.El milhojas se ha convertido en
+            un símbolo del arte pastelero francés con su presentación
+            artística. < br /> Mientras los panaderos experimentan con
+            deliciosas variaciones, añadiendo fruta, chocolate o caramelo a
+            las capas, el milhojas sigue encantando a los entusiastas de los
+            postres de todo el mundo.Saboree este manjar en capas y descubra
+            por qué el milhojas sigue siendo un favorito atemporal en el mundo
+            de la repostería francesa.`,
+        img: "./img/mille-feuille.jpg"
+    },
+    {
+        nombre: "Facturas y Masas Finas",
+        descripcion: `Tenemos una gran variedad de facturas y masas finas. <br />!No te
+            los pierdas!`,
+        img: "./img/facturas.jpg"
+    },
+    {
+        nombre: " Baguettes",
+        descripcion: ` Y como no podía faltar, tenemos, como siempre, los mejores
+            baguettes que vas a probar. <br />
+            ¡Cometelos calentitos!`,
+        img: "./img/baguette.jpg"
+    }
+]
+
+
+const productContainer = document.getElementById("product_container");
+
+for (let i = 0; i < products.length; i++) {
+    productContainer.innerHTML += `
+ <div class="pasteleria-grupo">
+          <img src=${products[i].img} alt="Cannoli" />
+
+          <p class="pasteleria-grupo-encabezado">
+            ${products[i].nombre}<br /><i class="fas fa-plus"></i>
+          </p>
+
+          <div class="pasteleria-grupo-cuerpo" id=${i}>
+          <p>
+           ${products[i].descripcion}
+            <br />
+            <a href="#">Pedir</a>
+          </p>
+          </div>
+        </div>
+
+`
+
+    const cards = document.querySelectorAll('.pasteleria-grupo');
+    const descriptions = document.querySelectorAll('.pasteleria-grupo-cuerpo');
+
+    // Agregar evento click a cada tarjeta
+    cards.forEach((card, index) => {
+        card.addEventListener('click', () => {
+            // Verificar si la descripción está visible
+            const isVisible = descriptions[index].style.display === 'block';
+
+            // Ocultar todas las descripciones
+            descriptions.forEach((desc) => {
+                desc.style.display = 'none';
+            });
+
+            // Si la descripción no estaba visible, mostrarla
+            if (!isVisible) {
+                descriptions[index].style.display = 'block';
+            }
+        });
+    });
+
+}
+
+// Obtener todas las tarjetas y descripciones
+
+
