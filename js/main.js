8aa2f09bb373ee2d6931e785611f3fb70c8561d6
Bernabe Suarez <bernatattoo@gmail.com>
2024-05-17 10:45:44 -0300

    clear js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,4 @@
-// // Pasteleria Acordeon
 
-// 
-//   const pasteleriaContainer = document.querySelector(".productos-grid");
-
-//   pasteleriaContainer.addEventListener("click", (e) => {
-//     const groupHeader = e.target.closest(".pasteleria-grupo");
-
-//     if (!groupHeader) return;
-
-//     const group = groupHeader.parentElement;
-//     const groupBody = group.querySelectorAll(".pasteleria-grupo-cuerpo");
-//     const icon = groupHeader.querySelector("i");
-
-//     // Transicion Icono
-//     icon.classList.toggle("fa-plus");
-//     icon.classList.toggle("fa-minus");
-
-//     // Transicionar visibilidad del cuerpo
-//     groupBody.classList.toggle("open");
-
-//     const otrosGrupos = pasteleriaContainer.querySelector(".pasteleria-grupo");
-
-//     otrosGrupos.forEach((otroGrupo) => { });
-//     if (otroGrupo !== group) {
-//       const otroGrupoCuerpo = otroGrupo.querySelector(
-//         ".pasteleria-grupo-cuerpo"
-//       );
-//       const otroIcono = otroGrupo.querySelector(
-//         ".pasteleria-grupo-encabezado i"
-//       );
-
-//       otroGrupoCuerpo.classList.remove("open");
-//       otroIcono.classList.remove("fa-minus");
-//       otroIcono.classList.add("fa-plus");
-//     }
-//   });
-// });
 
 document.addEventListener("DOMContentLoaded", () => {
   // Obtener todas las tarjetas y descripciones
